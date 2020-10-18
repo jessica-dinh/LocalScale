e83f5c644d80dae117deb00614700b208095ccd8
ackho <ackho@uw.edu>
2020-10-18 02:47:50 -0700

    added popup to service page

---
+++ src/service-page/service_nonuser.js
@@ -0,0 +1,64 @@
+"use strict";
+(function() {
+  window.addEventListener("load", init);
+
+  function init() {
+    id("offer-btn").addEventListener('click', btnClick);
+    id("close").addEventListener('click', close);
+  }
+
+  function btnClick() {
+    id("popup").classList.remove("hidden");
+  }
+
+  function close() {
+    id("popup").classList.add("hidden");
+  }
+
+
+
+  /** ------------------------------ Helper Functions  ------------------------------ */
+
+  /**
+   * Helper function to return the response's result text if successful, otherwise
+   * returns the rejected Promise result with an error status and corresponding text
+   * @param {object} response - response to check for success/error
+   * @return {object} - valid response if response was successful, otherwise rejected
+   *                    Promise result
+   */
+  function checkStatus(response) {
+    if (response.ok) {
+      return response;
+    } else {
+      throw Error("Error in request: " + response.statusText);
+    }
+  }
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /**
+   * Returns the first element that matches the given CSS selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} The first DOM object matching the query.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  /**
+   * Returns a new element with the given tag name.
+   * @param {string} tagName - HTML tag name for new DOM element.
+   * @returns {object} New DOM object for given HTML tag.
+   */
+  function gen(tagName) {
+    return document.createElement(tagName);
+  }
+
+})();
